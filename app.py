9994ff80a5cf26421e58070e63a6885e5ed75e46
prasadsawant7-spinach <prasad@spinachexperience.design>
2024-05-24 12:42:48 +0530

    Changes made

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+    return "Flask inside Docker using Docker Hub Private Repo!!"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
